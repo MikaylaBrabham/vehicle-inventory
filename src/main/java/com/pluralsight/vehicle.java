744c78abca8a1b810a1bb592f823a373a6693e0b
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-24 23:50:27 -0500

    added vehicle variables and plan to get constructors and getters and setters

--- src/main/java/com/pluralsight/vehicle.java
+++ src/main/java/com/pluralsight/vehicle.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
 public class vehicle {
+    //add given variables
+    double vehicleId;
+    String makeModel;
+    String color;
+    int odometerReading;
+    double price;
+
+    //add constructors
+
+
+    //add getters and setters
+
 }
